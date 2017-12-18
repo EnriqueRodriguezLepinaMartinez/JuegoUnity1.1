1e8355c4cc81b02a7b80a1d64fa90206feb7ec5e
EnriqueLepina <enriquelepina@gmail.com>
2017-12-18 12:23:54 +0100

    1 commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{	
+	public float speed = 4f;
+	private Rigidbody rb;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+	private int numberOfGameObjects;
+	
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winText.text = "";
+		numberOfGameObjects = GameObject.FindGameObjectsWithTag("PickUp").Length;
+	}
+	
+	/*void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}*/
+
+		/* Adaptacion para el acelerometro del movil */
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.acceleration.x;
+		float moveVertical = Input.acceleration.y;
+		transform.position += new Vector3 (moveHorizontal, 0.0f, moveVertical)* speed * Time.deltaTime;
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+	
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString();
+		if(count >= numberOfGameObjects)
+		{
+			winText.text = "YOU WIN!";
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Roll-a-ball
+===========
+
+[Unity 3D](http://unity3d.com)'s official tuturial #00 - Roll-a-ball.
+
+Tutorial is listed here: http://unity3d.com/learn/tutorials/projects/roll-a-ball
